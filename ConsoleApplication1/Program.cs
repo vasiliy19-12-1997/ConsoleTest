56ff85b20a22c168885d863f3cdaac47a57b6a1a
vasiliy19-12-1997 <Wasyok.ru@yandex.ru>
2023-05-28 09:06:11 +0500

    1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Основные типы int float
+            //целочисленные типы
+            byte b; //0 255
+            sbyte sb; // -128 127
+            short s;
+            ushort us;
+            int i;
+            uint ui;
+            long l = long.MaxValue;
+            ulong ul;
+            //числа с плавующей точкой
+            float f = 5.7f;
+            double d = 5.7;
+            //симовольный тип
+            char c = ' ';
+            //строковый тип
+            string str = "You are my soulmate";
+            //logic type
+            bool bl = true;
+            
+
+        }   
+    }
+}
