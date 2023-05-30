c12f943b5c279bab3ef447b567ff3df44a9a4958
Vasili <Wasyok.ru@yandex.ru>
2023-05-30 19:51:06 +0500

    1

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Security.Cryptography;
+using System.Security.Policy;
 
 namespace ConsoleApplication1
 {
@@ -64,12 +65,80 @@ namespace ConsoleApplication1
             // int x = 5, y = 2;
             // result = Convert.ToSingle(x) / y;
             // Console.WriteLine(result);
-            int trigger = 1;
-            bool triggerInBoolean = Convert.ToBoolean(trigger);
-            Console.WriteLine(triggerInBoolean);
-            int reconvert = Convert.ToInt32(triggerInBoolean);
-            Console.WriteLine(reconvert);
+            // int trigger = 1;
+            // bool triggerInBoolean = Convert.ToBoolean(trigger);
+            // Console.WriteLine(triggerInBoolean);
+            // int reconvert = Convert.ToInt32(triggerInBoolean);
+            // Console.WriteLine(reconvert);
+            // Console.WriteLine();
+            // Console.ReadLine();
             
+            //работа с консолью
+            
+            // string name;
+            // Console.Write("enter your name ");
+            // name = Console.ReadLine();
+            // Console.WriteLine($"Your name:{name}");
+            // int age;
+            // Console.Write("Enter your age");
+            // age = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine($"Your age is {age}");
+
+            // int A = 0;
+            // //                   1   + 2 + 1           1 + "1"  3*2   
+            // Console.WriteLine((++A) + 2 + 1 + (A++) + "1" +  (++A)*2);
+            
+            //  gameOfThrones
+            /*
+            float health;
+            int armor;
+            float demage;
+            int precentConventer = 100;
+            Console.Write("Enter health value: ");
+            health = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter armor value: ");
+            armor = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter demage value: ");
+            demage = Convert.ToInt32(Console.ReadLine());
+            
+            // health -= demage * armor / precentConventer;
+            health -= Convert.ToSingle(demage)/ precentConventer*armor;
+            Console.WriteLine($"Your hit damage: {demage}, You have health: {health}, your armor is {armor}");
+            */
+            
+           
+            // store
+            /*
+            int money;
+            int food;
+            int foodUnikPrice = 10;
+            bool isAbleToPay;
+            Console.WriteLine("Welcome to store, Today all food on a special" + foodUnikPrice + "coins");
+            Console.Write("How many money you have?");
+            money = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько еды вам  нужно");
+            food = Convert.ToInt32(Console.ReadLine());
+            isAbleToPay = money >= food * foodUnikPrice;
+            food *= Convert.ToInt32(isAbleToPay);
+            money -= food * foodUnikPrice;
+            Console.WriteLine($"you have{food} and coins: {money}");
+            */
+
+            string dayOfWeek;
+            Console.WriteLine("Enter day of week");
+            dayOfWeek = Console.ReadLine();
+            if (dayOfWeek == "Понедельник")
+            {
+                Console.WriteLine("Проходим курсы");
+            }
+
+            else if (dayOfWeek == "вторник")
+            {
+                Console.WriteLine("Делаем дз");
+            }
+
         }   
     }
 }
